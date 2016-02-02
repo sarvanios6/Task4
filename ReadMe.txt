86e7e144bb800d0306465179d17d93bd331b7629
sarvan <sarvanios6@gmail.com>
2016-02-02 14:51:26 +0530

    project added

---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+.csv FILE DATA content read with using json format.
+
+* convert the data .csv to Json after that using Json Parser to read the content in this program.
+* set the path of file reader from .json format file in the parser. when read the Json data will be stored to JsonArray.
+* JsonArray data's filter to print the first and last name list in console for who is consume the current product. here using the if (condition) get and filter the current product name for the Json file. 
---
+++ src/JsonReader.java
@@ -0,0 +1,61 @@
+
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class JsonReader {
+    public static void main(String[] args) throws Exception {
+
+        JSONParser parser = new JSONParser();
+
+        try {
+            System.out.println("Enter Product Name : \n");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String myProductName = br.readLine();
+            System.out.println("Your Product Name :"+myProductName);
+            String currentProduct = "";
+            Object obj = parser.parse(new FileReader("/Users/Ramdhas/Desktop/data.json"));
+            JSONArray jsonArray = (JSONArray) obj;
+            for (int i = 0; i < jsonArray.size(); i++) {
+
+                JSONObject jsonObjectRow = (JSONObject) jsonArray.get(i);
+                currentProduct = (String) jsonObjectRow.get("productname");
+                if(myProductName.contains(currentProduct))
+                {
+                    System.out.println(jsonObjectRow.get("firstname") + " " + jsonObjectRow.get("lastname"));
+                }
+            }
+
+
+            System.out.println("Enter Last Name : \n");
+
+            String myLastName = br.readLine();
+            System.out.println("Your Product Name :"+myLastName);
+            String lastname = "";
+
+            for (int i = 0; i < jsonArray.size(); i++) {
+
+                JSONObject jsonObjectRow = (JSONObject) jsonArray.get(i);
+                lastname = (String) jsonObjectRow.get("lastname");
+                if(myLastName.contains(lastname))
+                {
+                    System.out.println(jsonObjectRow.get("productname") + " ");
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+    }
+}
